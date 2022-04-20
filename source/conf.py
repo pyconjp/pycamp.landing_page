01c80681b07c6f1340e2e23cbef4c58a9bc8671c
ftnext <takuyafjp+develop@gmail.com>
2022-04-21 01:16:42 +0900

    [chore] Hide sidebar (close #4)

--- source/conf.py
+++ source/conf.py
@@ -53,6 +53,9 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_theme_options = {
+    "nosidebar": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
