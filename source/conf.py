56c3a81e4c689d1aa8c4ec544b78af0af953b897
ftnext <takuyafjp+develop@gmail.com>
2022-08-13 11:29:55 +0900

    [chore] Set favicon

--- source/conf.py
+++ source/conf.py
@@ -65,5 +65,6 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_favicon = "_static/favicon.png"
 
 # -- Extension configuration -------------------------------------------------
