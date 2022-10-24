c00b62c4da2568d3c9e2e5fd524a5b3644426073
ftnext <takuyafjp+develop@gmail.com>
2022-10-25 00:41:30 +0900

    [refactor] Overriding starttag makes impl simple (close #33)

--- source/extensions/add_attributes.py
+++ source/extensions/add_attributes.py
@@ -6,40 +6,21 @@ from sphinx.writers.html import HTMLTranslator
 class PatchedHTMLTranslator(HTMLTranslator):
     """aタグのリンクを別タブで開くようにパッチを当てたtranslator
 
-    ref: https://stackoverflow.com/a/61669375
+    ref: https://stackoverflow.com/a/67153583
     """
-    # Copied from Sphinx v4.5.0
-    def visit_reference(self, node: Element) -> None:
-        atts = {'class': 'reference'}
-        if node.get('internal') or 'refuri' not in node:
-            atts['class'] += ' internal'
-        else:
-            atts['class'] += ' external'
-            # ----- patched -----
+
+    def starttag(self, node, tagname, *args, **atts):
+        if (
+            tagname == "a"
+            and "target" not in atts
+            and (
+                "external" in atts.get("class", "")
+                or "external" in atts.get("classes", [])
+            )
+        ):
             atts["target"] = "_blank"
             atts["rel"] = "noopener noreferrer"
-            # ----- patched end -----
-        if 'refuri' in node:
-            atts['href'] = node['refuri'] or '#'
-            if self.settings.cloak_email_addresses and atts['href'].startswith('mailto:'):
-                atts['href'] = self.cloak_mailto(atts['href'])
-                self.in_mailto = True
-        else:
-            assert 'refid' in node, \
-                   'References must have "refuri" or "refid" attribute.'
-            atts['href'] = '#' + node['refid']
-        if not isinstance(node.parent, nodes.TextElement):
-            assert len(node) == 1 and isinstance(node[0], nodes.image)
-            atts['class'] += ' image-reference'
-        if 'reftitle' in node:
-            atts['title'] = node['reftitle']
-        if 'target' in node:
-            atts['target'] = node['target']
-        self.body.append(self.starttag(node, 'a', '', **atts))
-
-        if node.get('secnumber'):
-            self.body.append(('%s' + self.secnumber_suffix) %
-                             '.'.join(map(str, node['secnumber'])))
+        return super().starttag(node, tagname, *args, **atts)
 
 
 def centering_header_elements_text(doctree):
